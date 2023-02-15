df93f340a7bc77166abb008dbb57ba83a5b46baa
Anthony-Hoo <tonyhoo2000@hotmail.com>
2023-02-15 16:40:51 +0800

    feat: 增加 docker 部署方式

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 
 
+
 app = Flask(__name__)
 app.config.from_object("config")
-app.secret_key = '1234567'
+app.secret_key = 'd25ba7735e13a52601fd339fd28f9d869b5ba3759e9e464d3911224820f3a22a'
 
 
 
@@ -16,7 +17,7 @@ def giveBackURL():
     if(request.method == "GET"):
         # 从./db/character.db中随机取出一条数据，以json返回
         import sqlite3
-        conn = sqlite3.connect('./db/character.db')
+        conn = sqlite3.connect('/var/www/character.db')
         c = conn.cursor()
         c.execute("SELECT * FROM character WHERE id >= (ABS(RANDOM()) % (SELECT MAX(rowid) FROM character)) LIMIT 1;")
         result = c.fetchone()
@@ -29,10 +30,10 @@ def giveBackURL():
             'audio': result[4],
         })
 
-
+def run():
+    app.run(host="*0.0.0.0", port=8000)
 
 
 if __name__ == '__main__':
     
-    app.run(host="127.0.0.1",
-            port=8000)
+    app.run(host="127.0.0.1", port=8000)
